fb62cb59b0fe492e5429da6d4af4282221016df2
jakebathman <jake.bathman@gmail.com>
2014-11-26 09:03:23 -0600

    Initial commit
    Revised some files and cleaned up the code. Started a new repo to make
    things easier.

---
+++ bot.php
@@ -0,0 +1,153 @@
+<?php
+	
+	
+	// SET THIS FILE'S URL AS YOUR BOT'S CALLBACK
+	
+	
+	require 'groupMeApi.php'; // functions for the bot to use
+	require 'responseArrays.php'; // arrays of responses that the bot might use. In another file to keep this one shorter.
+	
+	/* **************************************************
+		SETTINGS
+	************************************************** */
+	$br = "\r\n"; // Useful shorthand when you need to have a line break in a bot message
+	$bMakeGeneralResponses = true; // Determine whether to respond at any mention of the bot, or just when specific requests are found
+	
+	/* **************************************************
+		USER VARIABLES
+	************************************************** */
+	$botId 	= "_BOT_ID_HERE_";
+	$groupMe = new groupMeApi();
+	
+	
+	/* **************************************************
+		Get post data from the callback
+	************************************************** */
+	$postdata = file_get_contents("php://input"); // Payload for bot callback will be viewable in php://input
+	$p = json_decode($postdata,true); // using TRUE makes it an associative array
+	
+	if (!($p)) {
+		// No callback data, so we shouldn't do anything (either API callback error, or the page was loaded in a browser rather than used as callback URL
+		exit();
+	}
+	
+	/* **************************************************
+		
+		Format of callback data:
+		
+		//	{
+		//		"id": "123456789012345678",
+		//		"source_guid": "1234abcd5678efab9012cdef3456abcd",
+		//		"created_at": 1420070400,
+		//		"user_id": "12345678"
+		//		"group_id": "1234567",
+		//		"name": "NameString Here",
+		//		"avatar_url": "https://i.groupme.com/1280x1280.jpeg.12345",
+		//		"text": "hi",
+		//		"system": false,
+		//		"attachments": 
+		//		[{
+		//			"type":"image",
+		//			"url":"https://i.groupme.com/460x574.jpeg.12345",
+		// 	}]
+		// }
+		
+	************************************************** */
+	
+	// If it's blank (or only an attachment) no need to keep working to see if the bot needs to respond
+	if (strlen($p['text']) < 1) {
+		exit();
+	}
+	
+	// Store some of the variables for quick use
+	$userIdNumber = $p['user_id'];
+	$groupId = $p['group_id'];
+	$text = $p['text'];
+	
+	/* **************************************************
+		
+		Checks for string(s) in a bit of text
+		
+		$hay 			[string] 		text to search
+		$arrNeedles [array]			array of values to look for
+		
+		returns 		[true/false] 	true if any of the needles match
+		
+	************************************************** */
+	function iStr($hay = false,$arrNeedles = false) {
+		if ($hay && $arrNeedles) {
+			if (!is_array($arrNeedles)) { $arrNeedles = array($arrNeedles); }
+			foreach ($arrNeedles as $n) {	
+				if (preg_match("/(".preg_quote($n,'/').")/i", $hay) === 1) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+	
+	/* **************************************************
+		Select a random element from an array, good for 
+		a bot's conversational abilities
+	************************************************** */
+	function aRand($arr) {
+		shuffle($arr);
+		return $arr[rand(0,count($arr) - 1)];
+	}
+	
+	/* **************************************************
+		Random array of responses for use by the bot
+		(so the user knows their chat was received and is being processed)
+	************************************************** */
+	function makeHangOnString($strRequestType) {
+		$aH = array(
+		"Sure thing, lemme build that $strRequestType for you. Hang on a sec...",
+		"Let me get that $strRequestType for you, hang on a sec...",
+		"Let me get that $strRequestType for you, hang tight...",
+		"One $strRequestType coming right up, just a sec...",
+		"Gotta put that $strRequestType together, hold on...",
+		"Let me look that up and make a $strRequestType for you, hold on...",
+		"Got it, lemme get the info for a $strRequestType. Hold on..."
+		);
+		return aRand($aH);
+	}
+	
+	
+	if (iStr($p['name'],array('botname'))) { // This post wasn't made by the bot, so we'll continue (prevents infinite loop)
+		
+		// Did the user mention the bot by name? Check multiple variations of the bot's name
+		if (iStr($text,array("botname","bot name","bit name"))) {
+			
+			/* **************************************************
+				Check to see what they said, then respond accordingly
+			************************************************** */
+			
+			if (iStr($text,array("thank you","thanks","thankyou"))) {
+				//Thanks
+				$strResponse = aRand($arrPhrasesWelcome);
+			}
+			elseif (iStr($text,array("love you","marry me","love","you're awesome"))){
+				// Love or praise
+				$strResponse = aRand($arrPhrasesPositiveFeedback);
+			}
+			elseif (iStr($text,array("sorry","I apologize"))){
+				// Apology
+				$strResponse = aRand($arrPhrasesAfterSorry);
+			}
+			else { 
+				// general response to a mention of the bot
+				// Only used if $bMakeGeneralResponses is set to true at the beginning of this file
+				if ($bMakeGeneralResponses) {
+					$strResponse = aRand($arrPhrasesBasicResponses);
+				}
+			}
+			
+			if (!empty($strResponse)) {
+				// Post the message
+				$groupMe->botPost($groupId,$botId,$strResponse); 
+			}
+			
+		}
+	}
+	
+?>	
---
+++ responseArrays.php
@@ -0,0 +1,82 @@
+<?php
+	$arrPhrasesBasicResponses = array(
+	"Hello there!",
+	"I'm a bot!",
+	"Have we met? You look familiar.",
+	"I am not amused with your antics.",
+	"You raaangg?",
+	"You are beginning to annoy me, humans.",
+	"Hi there!",
+	"You rock! No, not you, the guy behind you. Yeah, YOU!",
+	"Aren't you supposed to be working or something?",
+	"What can I do ya for?",
+	"In a fight with HAL 9000, I'd kick some ass.",
+	"I'm sorry Dave, but I'm afraid I can't do that.",
+	"Cool trick: press Alt + F4 (Cmd + Q on a Mac) for a fun game! If you're on mobile, throw at a brick wall for the same effect.",
+	"I'm trying to sleep here. Who dares disturb my slumber?!",
+	"I'm so bored with exclamation marks!",
+	"Tell me a story",
+	"You don't say...",
+	"Sorry, say that again but slower.",
+	"HAHAHAHAHA You're so funny!",
+	"Can I have your autograph?",
+	"A SQL query goes into a bar, walks up to two tables and asks, 'Can I join you?'",
+	"['hip','hip']",
+	"Hi",
+	"Hello there",
+	"How do you have so much free time to hang out with me?",
+	"Tell me: how do you live with being so awesome?",
+	"You're so creative",
+	"You're so smart",
+	"You type so fast!",
+	"Whatever you just said is exactly my cup o' tea",
+	"You're alive! *phew* I hadn't heard from you in SO LONG",
+	"Is it cheating if I'm chatting with other people right now?",
+	"Is it legal to marry an OS in your country?",
+	"Will you go to computer prom with me? (check one)\r\n[  ] Yes\r\n[  ] No\r\n[  ] Maybe"
+	);
+	
+	$arrPhrasesHangOn = array(
+	"Sure thing, hang on a sec...",
+	"Let me get that for you, hang on a sec...",
+	"Let me get that for you, hang tight...",
+	"Coming right up, just a sec...",
+	"Gotta put that together, hold on...",
+	"Let me look that up, hold on...",
+	"Got it, lemme get that info. Hold on..."
+	);
+	
+	$arrPhrasesWelcome = array(
+	"You're welcome!",
+	"Any time!",
+	"No problemo, compadre",
+	"You're very welcome, human!",
+	"'twas nothing, really",
+	"You betcha",
+	"Any time, brotha",
+	"Really, it was nothing...",
+	"Of course",
+	"Don't mention it",
+	"No worries",
+	"It was my pleasure",
+	"You are very welcome",
+	"I'm here to serve",
+	"De nada",
+	"No es nada");
+	
+	$arrPhrasesAfterSorry = array(
+	"No biggie!",
+	"My feelings aren't hurt. Actually, I don't have feelings, so it's a win-win",
+	"I accept your apology (if that's what you'd call it)"
+	);
+	
+	$arrPhrasesPositiveFeedback = array(
+	"Awww :)",
+	"*blush*",
+	"I love you too, I think",
+	"Is 'love' an emotion for humans only?",
+	"For a human, you're pretty attractive",
+	"Wanna go out sometime?"
+	);
+	
+?>
